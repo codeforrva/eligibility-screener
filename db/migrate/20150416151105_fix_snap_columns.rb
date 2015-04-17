2beaeb186ca559f14b73d203be3e9c42fe1f39ba
Tony Gambone <tonygambone@gmail.com>
2015-04-17 12:41:33 -0400

    Add ruby version to Gemfile and fix a migration that is failing on postgres.

--- db/migrate/20150416151105_fix_snap_columns.rb
+++ db/migrate/20150416151105_fix_snap_columns.rb
@@ -1,8 +1,17 @@
 class FixSnapColumns < ActiveRecord::Migration
   def change
-    change_column :snap_eligibilities, :snap_dependent_no, :integer
-    change_column :snap_eligibilities, :snap_gross_income, :integer
-    change_column :snap_eligibility_seniors, :snap_dependent_no, :integer
-    change_column :snap_eligibility_seniors, :snap_gross_income, :integer
+    if ActiveRecord::Base.connection.adapter_name = 'PostgreSQL'
+      # postgresql needs to cast from string to integer
+      change_column :snap_eligibilities, :snap_dependent_no, 'integer USING CAST("snap_dependent_no" AS integer)'
+      change_column :snap_eligibilities, :snap_gross_income, 'integer USING CAST("snap_gross_income" AS integer)'
+      change_column :snap_eligibility_seniors, :snap_dependent_no, 'integer USING CAST("snap_dependent_no" AS integer)'
+      change_column :snap_eligibility_seniors, :snap_gross_income, 'integer USING CAST("snap_gross_income" AS integer)'
+    else
+      # sqlite3
+      change_column :snap_eligibilities, :snap_dependent_no, :integer
+      change_column :snap_eligibilities, :snap_gross_income, :integer
+      change_column :snap_eligibility_seniors, :snap_dependent_no, :integer
+      change_column :snap_eligibility_seniors, :snap_gross_income, :integer
+    end
   end
 end
